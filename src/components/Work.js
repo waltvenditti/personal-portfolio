6426de2edddcc402a165e726c63c29ebefb3bb8f
WaltVenditi <waltvenditti@gmail.com>
2022-03-16 11:22:57 -0400

    implemented work cards component, minus actual project screenshots

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,17 +1,67 @@
 import "../styles/Work.css";
 import WorkCard from "./WorkCard";
+import React, { useState } from "react";
 
 function Work() {
-    return (
-        <div className="workDiv">
-            <WorkCard />
-            <WorkCard />
-            <WorkCard />
-            <WorkCard />
-            <WorkCard />
-            <WorkCard />
-        </div>
-    )
+  const [projInfo1, setProjInfo1] = useState(
+    {
+      projTitle: "Project 1",
+      githubLink: "https://www.zerohedge.com",
+      githubLive: "https://www.economist.com",
+      projDesc: "description of the project goes here."
+    }
+  );
+  const [projInfo2, setProjInfo2] = useState(
+    {
+      projTitle: "Project 2",
+      githubLink: "https://www.zerohedge.com",
+      githubLive: "https://www.economist.com",
+      projDesc: "description of the project goes here."
+    }
+  );
+  const [projInfo3, setProjInfo3] = useState(
+    {
+      projTitle: "Project 3",
+      githubLink: "https://www.zerohedge.com",
+      githubLive: "https://www.economist.com",
+      projDesc: "description of the project goes here."
+    }
+  );
+  const [projInfo4, setProjInfo4] = useState(
+    {
+      projTitle: "Project 4",
+      githubLink: "https://www.zerohedge.com",
+      githubLive: "https://www.economist.com",
+      projDesc: "description of the project goes here."
+    }
+  );
+  const [projInfo5, setProjInfo5] = useState(
+    {
+      projTitle: "Project 5",
+      githubLink: "https://www.zerohedge.com",
+      githubLive: "https://www.economist.com",
+      projDesc: "description of the project goes here."
+    }
+  );
+  const [projInfo6, setProjInfo6] = useState(
+    {
+      projTitle: "Project 6",
+      githubLink: "https://www.zerohedge.com",
+      githubLive: "https://www.economist.com",
+      projDesc: "description of the project goes here."
+    }
+  );
+
+  return (
+    <div className="workDiv">
+      <WorkCard cardInfo={projInfo1} />
+      <WorkCard cardInfo={projInfo2} />
+      <WorkCard cardInfo={projInfo3} />
+      <WorkCard cardInfo={projInfo4} />
+      <WorkCard cardInfo={projInfo5} />
+      <WorkCard cardInfo={projInfo6} />
+    </div>
+  );
 }
 
 export default Work;
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,11 +1,37 @@
 import "../styles/Work.css";
+import imgGitHub from "../images/github-notext.svg";
+import imgSeeLive from "../images/monitor-eye.png";
 
-function WorkCard() {
-    return (
-        <div className="workCardDiv">
-
+function WorkCard(props) {
+  return (
+    <div className="workCardDiv">
+      <div className="wcdGridItem wcdScreenshot">
+        <span>project screenshot</span>
+      </div>
+      <div className="wcdGridItem wcdDesc">
+        <div className="wcdDescHeader">
+          <p>{props.cardInfo.projTitle}</p>
+          <a
+            className="imgHyperlink"
+            href={props.cardInfo.githubLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={imgGitHub} alt="github logo" />
+          </a>
+          <a 
+            href={props.cardInfo.githubLive} target="_blank" 
+            rel="noreferrer"
+          >
+            <img src={imgSeeLive} alt="see live icon" />
+          </a>
         </div>
-    )
+        <div className="wcdDescText">
+          <p>{props.cardInfo.projDesc}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WorkCard;
