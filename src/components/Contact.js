ab981eb45b449298f39daefd541a6369c3d77472
WaltVenditi <waltvenditti@gmail.com>
2022-03-16 19:24:55 -0400

    added text, image, and basic style to contact self, still in-progress tho

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,25 @@
 import "../styles/Contact.css";
+import imgCompScreen from "../images/computer-screen.jpeg";
+import imgPhoneIcon from "../images/phone.png";
 
 function Contact() {
-    return (
-        <div className="contactDiv">
-
+  return (
+    <div className="contactDiv">
+      <div className="contactInfoDiv">
+        <p>Contact Me</p>
+        <p>Please get in touch if you think our work could be mutually beneficial.</p>
+        <p>1234 Random Road</p>
+        <p>Somewhere, Ohio 12345</p>
+        <div className="phoneInfo">
+          <img src={imgPhoneIcon} />
+          <span>555-555-5555</span>
         </div>
-    )
+      </div>
+      <div className="contactImgDiv">
+        <img src={imgCompScreen} />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
