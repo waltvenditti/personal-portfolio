ab886bcb4c5acb115378d23f353d7a47ba9376ad
WaltVenditi <waltvenditti@gmail.com>
2022-03-23 10:42:04 -0400

    essentially done with tablet view. Will not implement the off-set style for the top section.

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
         <img alt="scientist looking through microscope" src={imgScientist} />
         <div className="aboutInfoDiv">
           <h2>About Me</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, urna consequat felis vehicula class ultricies mollis dictumst, aenean non a in donec nulla. Phasellus ante pellentesque erat cum risus consequat imperdiet aliquam, integer placerat et turpis mi eros nec lobortis taciti, vehicula nisl litora tellus ligula porttitor metus. </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, urna consequat felis vehicula class ultricies mollis dictumst, aenean non a in donec nulla. Phasellus ante pellentesque erat risus consequat imperdiet aliquam, integer placerat et turpis mi eros nec lobortis taciti, vehicula nisl litora tellus ligula porttitor metus. </p>
           <div className="aboutButtonDiv">
             <a href="https://github.com/waltvenditti" target="_blank" rel="noreferrer">
               <img alt="GitHub link" src={imgGitHub}/>
